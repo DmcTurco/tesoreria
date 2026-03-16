43b24b99ceacf9af6fb26070d599e938d1ff30f5
David Moises Mamani CHino <moisesanibal@hotmail.com>
2026-03-16 08:46:25 -0500

    build

--- public_html/routes/web.php
+++ public_html/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/terminals/tesoreria/{any?}', function () {
+    return file_get_contents(public_path('terminals/tesoreria/index.html'));
+})->where('any', '.*');
